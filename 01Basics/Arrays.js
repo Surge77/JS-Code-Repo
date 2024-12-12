3989cffe6edf17136af5d6374173e7b5f78609db
Surge77 <133491633+Surge77@users.noreply.github.com>
2024-12-12 13:20:02 +0000

    Arrays1

--- 01Basics/Arrays.js
+++ 01Basics/Arrays.js
@@ -3,22 +3,23 @@
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
+// Another way of creating an array
 const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
+// myArr.push(6) -> adding element to last
+// myArr.push(7) -> removing the last element 
 // myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) -> Adds element at the start of the array
+// myArr.shift() -> removes the first element in the array
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9)); -> returns boolean if value exists or not
+// console.log(myArr.indexOf(3)); -> returns -1 if the index of the number doesn't exists
 
-// const newArr = myArr.join()
+// const newArr = myArr.join() -> joins the array and converts into string
 
 // console.log(myArr);
 // console.log( newArr);
@@ -26,14 +27,15 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // slice, splice
 
-console.log("A ", myArr);
+console.log("A ", myArr); 
 
-const myn1 = myArr.slice(1, 3)
+//Extracts a portion of an array without modifying the original array.
+const myn1 = myArr.slice(1, 3) 
 
 console.log(myn1);
 console.log("B ", myArr);
 
-
-const myn2 = myArr.splice(1, 3)
+//Adds, removes, or replaces elements in an array and modifies the original array.
+const myn2 = myArr.splice(1, 3) // Removes 3 elements starting from index 1 of myArr and returns the removed elements.
 console.log("C ", myArr);
-console.log(myn2);
+console.log(myn2); //Printing the removed elements returns by the splice
