8c1cfb739ea1e6c7cfd4e83e6b8cdcf22af812e0
Surge77 <133491633+Surge77@users.noreply.github.com>
2024-12-12 16:26:05 +0000

    Functions DONE

---
+++ 02Basics/Functions.js
@@ -0,0 +1,75 @@
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName() -> Calling the function or referencing
+
+// function addTwoNumbers(number1, number2){ -> these are parameters
+
+//     console.log(number1 + number2);
+// }
+
+// addTwoNumbers(3,4); -> These are arguments
+ 
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+// ... -> this is known as rest operator as well as spread operator name depends on where we use it
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+//passing object to a function
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+//passing arrays into a function
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
