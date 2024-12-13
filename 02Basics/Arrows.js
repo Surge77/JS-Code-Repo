cb0caa8f5673fbd8824a2b3afb7a7cd0190706ac
Surge77 <133491633+Surge77@users.noreply.github.com>
2024-12-13 07:53:00 +0000

    Arrows DONE

--- 02Basics/Arrows.js
+++ 02Basics/Arrows.js
@@ -4,7 +4,7 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(this); // this refers to current context
     }
 
 }
@@ -15,6 +15,7 @@ const user = {
 
 // console.log(this);
 
+//this doesn't work inside the functions
 // function chai(){
 //     let username = "hitesh"
 //     console.log(this.username);
@@ -35,14 +36,20 @@ const chai =  () => {
 
 // chai()
 
+
+//Function declaration using arrows
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
 // }
 
 // const addTwo = (num1, num2) =>  num1 + num2
 
+//This is implicit return
+//When we use curly braces we need to write return
+//When we use parantheses we don't need to write the return keyword
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
+//When we need to return objects through arrow function we need to surround the object in parantheses
 const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
